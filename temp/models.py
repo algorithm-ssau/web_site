44a861f9b8310b3f74e2b42da94b6504b4bd00fd
Alex S <47791266+cjoz@users.noreply.github.com>
2019-04-04 19:58:38 +0400

    Changes to model

--- temp/models.py
+++ temp/models.py
@@ -1,14 +1,29 @@
-
+﻿
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    """
+    Информация о туристическом объекте.
+    """
+    SIGHT_TYPES = (
+        ('a', 'Классическая архитектура'), # до 1940-50-х
+        ('b', 'Современная архитектура'),
+        ('c', 'Скульптура'),
+        ('d', 'Музеи'),
+        ('e', 'Театры'),
+        ('f', 'Улицы, площади'),
+        ('g', 'Парки')
+    )
+    
+    name = models.CharField(max_length=200) 
+    text = models.TextField()   # описание
     city = models.CharField(max_length=50)
-    type = models.CharField(max_length=100)
-
+    type = models.CharField(max_length=1, choices=SIGHT_TYPES)
+    times_visited = models.IntegerField()
     def __str__(self):
-        return self.title
+        return self.name
+
+
