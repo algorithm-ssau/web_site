effbe4b3f84b4f8865d5db6631c97f1a959caf64
Alex S <47791266+cjoz@users.noreply.github.com>
2019-04-05 22:30:41 +0400

    created index.html

--- temp/views.py
+++ temp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+﻿from django.shortcuts import render
+from .models import Post
 # Create your views here.
+def index(request):
+    """
+    Функция отображения для главной страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_sights=Post.objects.all().count()
+    
+    # Доступные книги (статус = 'a')
+    most_visited=Post.objects.order_by('-times_visited').first().name
+    mv_city = Post.objects.order_by('-times_visited').first().city
+    
+    # Отрисовка HTML-шаблона index.html с данными внутри 
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_sights':num_sights, 'most_visited':most_visited, 'mv_city':mv_city}
+    )
